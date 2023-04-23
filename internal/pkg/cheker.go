687c84cd4f700e7be6f1079c0e5c71acb8581327
bshayakh <shayakhmet.bauyrzhan@gmail.com>
2023-04-23 16:20:07 +0600

    add check user name's to emptitys

--- internal/pkg/cheker.go
+++ internal/pkg/cheker.go
@@ -2,32 +2,32 @@ package pkg
 
 import (
 	"errors"
-	"fmt"
+	"regexp"
 	"strings"
 	"testForum/internal/models"
 )
 
 var (
-	ErrInvalidEmail    = errors.New("invalid email address")
-	ErrInvalidPassword = errors.New("invalid password")
-	ErrInvalidUsername = errors.New("invalid username")
+	ErrInvalidEmail    = errors.New("email must consists from letters, at and dot")
+	ErrInvalidPassword = errors.New("Password have to contain at least 8 characters but no more than 20; include number, lowercase, uppercase and symbol")
+	ErrInvalidUsername = errors.New("Name must consists from letters")
 )
 
 func CheckUserInfo(user models.User) error {
-	// if !regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`).MatchString(user.Email) {
-	// 	fmt.Println("111")
-	// 	return ErrInvalidEmail
-	// }
+	if !regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`).MatchString(user.Email) {
+		return ErrInvalidEmail
+	}
+	if user.User_name == "" {
+		return ErrInvalidUsername
+	}
 
 	for _, w := range user.User_name {
 		if w < 32 || w > 126 {
-			fmt.Println("222")
 			return ErrInvalidUsername
 		}
 	}
 
 	if !checkPassword(user.Password) {
-		fmt.Println("333")
 		return ErrInvalidPassword
 	}
 
@@ -41,18 +41,15 @@ func checkPassword(password string) bool {
 	symbols := "!@#$%^&*()_-+={[}]|\\:;<,>.?/"
 
 	if len(password) < 8 || len(password) > 20 {
-		fmt.Println("444")
 		return false
 	}
 
 	if !contains(password, numbers) || !contains(password, lowerCase) || !contains(password, upperCase) || !contains(password, symbols) {
-		fmt.Println("555")
 		return false
 	}
 
 	for _, w := range symbols {
 		if w < 32 || w > 126 {
-			fmt.Println("666")
 			return false
 		}
 	}
@@ -62,10 +59,9 @@ func checkPassword(password string) bool {
 func contains(s, checkSymbols string) bool {
 	for _, w := range checkSymbols {
 		if strings.Contains(s, string(w)) {
-			fmt.Println("777")
 			return true
 		}
 	}
-	fmt.Println("888")
+
 	return false
 }
